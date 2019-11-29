4cb91b79ae601e77cd6120390ca1166978f6ac06
Keeano Martin <keeanomartin@Keeanos-iMac.local>
2019-11-29 07:25:59 -0800

    Initial commit for core iOS FarmEster hosting browser

---
+++ FarmEster/FarmEster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FarmEster
+//
+//  Created by Keeano Martin on 11/29/19.
+//  Copyright © 2019 Keeano Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
