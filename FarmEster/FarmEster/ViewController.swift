54070944d47809ad8079e40c3b19ed823f9be256
Keeano Martin <keeanomartin@Keeanos-iMac.local>
2019-12-01 22:49:14 -0800

    update to ios to map to old version for this current code base version 3.0.0

--- FarmEster/FarmEster/ViewController.swift
+++ FarmEster/FarmEster/ViewController.swift
@@ -7,14 +7,42 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKNavigationDelegate{
+    
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        //load up the application URL
+        let url = URL(string: "https://farmestar.herokuapp.com")
+        //Setup the DataStore for LocalStorage
+        let webConfig = webView.configuration
+        //Persist localStorage through WKWebView
+        webConfig.websiteDataStore = WKWebsiteDataStore.default()
+        //Launch WebView to MainStoryboard
+        webView.load(URLRequest(url: url!))
     }
-
-
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
+//    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+//        title = webView.title
+//        if(webView.url?.absoluteString == "https://app.getpulse.co/devices"){
+//            let script = "localStorage.getItem('userName')"
+//            webView.evaluateJavaScript(script){ (result, error) in
+////                if let result = result {
+//                    //register device to onesignal via result
+////                    let str = String(describing: result)
+////                    OneSignal.setEmail(str);
+////                }
+//            }
+//        }
+//    }
 }
 
